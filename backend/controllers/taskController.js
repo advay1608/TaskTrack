3ec33d8f896be8e0c8aa1807c7c9069acc8cb651
advay1608 <advaychavan2024@gmail.com>
2026-01-16 02:18:07 +0530

    fixed task creation issue

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,18 +1,20 @@
 import Task from "../models/Task.js";
 
-export const createTask = async(req, res) => {
-    try{
-        const { title, priority } = req.body;
-        const task = await Task.create({
-            title,
-            description,
-            priority
-        });
-        res.status(201).json(task);
-    } catch(error){
-        res.status(500).json({ message: "Task creation failed", error });
+export const createTask = async (req, res) => {
+  try {
+    const { title, priority } = req.body;
+
+    const task = await Task.create({
+      title,
+      priority
+    });
+
+    res.status(201).json(task);
+    }   catch (error) {
+    res.status(500).json({ message: "Task creation failed", error });
     }
 };
+
 
 export const getTasks = async(req, res) => {
     try{
